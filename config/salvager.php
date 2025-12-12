2533ab19254ca1f39dfe605288e2202e0767caab
kawax <kawaxbiz@gmail.com>
2025-12-12 18:33:11 +0900

    Update salvager.php

--- config/salvager.php
+++ config/salvager.php
@@ -3,6 +3,9 @@
 return [
     'playwright' => [
         // Playwright options can be added here.
+
+        // 'executable_path' => 'node',
+        // 'logger' => null,
     ],
 
     'screenshots' => storage_path('salvager/screenshots'),
