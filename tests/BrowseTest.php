ac69c2e6e456af7549ec59b262b0280c91037113
kawax <kawaxbiz@gmail.com>
2025-12-12 23:29:35 +0900

    Update BrowseTest.php

--- tests/BrowseTest.php
+++ tests/BrowseTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Mockery;
 use PlaywrightPhp\Resources\Page;
 use Revolution\Salvager\Facades\Salvager;
 
@@ -21,5 +22,24 @@ class BrowseTest extends TestCase
 
         $this->assertEquals('https://example.com/', $url);
         $this->assertGreaterThan(1, mb_strlen($text));
+    }
+
+    public function test_facade()
+    {
+        Salvager::expects('browse')
+            ->with(Mockery::type('callable'))
+            ->once()
+            ->andReturnNull();
+
+        Salvager::browse(function (Page $page) use (&$url, &$text) {
+            $page->goto('https://example.com/');
+            $page->screenshot(['path' => config('salvager.screenshots').'playwright-test.png']);
+
+            $url = $page->url();
+            $text = $page->querySelector('p')?->innerText();
+        });
+
+        $this->assertNull($url);
+        $this->assertNull($text);
     }
 }
