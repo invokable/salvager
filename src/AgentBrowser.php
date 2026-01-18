a54293b8d5e420f90a78978b2d7135ea800ee441
kawax <kawaxbiz@gmail.com>
2026-01-18 10:22:24 +0900

    Update AgentBrowser.php

--- src/AgentBrowser.php
+++ src/AgentBrowser.php
@@ -17,10 +17,10 @@ class AgentBrowser
     {
         // Install chromium
         if ($install_deps = config('salvager.agent-browser.install.deps')) {
-            Process::path(base_path())->run($install_deps);
+            Process::path(base_path())->timeout(60)->run($install_deps)->throw();
         }
         if ($install_chromium = config('salvager.agent-browser.install.chromium')) {
-            Process::path(base_path())->run($install_chromium);
+            Process::path(base_path())->timeout(60)->run($install_chromium)->throw();
         }
 
         $cmd = collect([
